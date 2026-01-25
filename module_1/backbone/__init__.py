aefd183c6c848dd4dc85c0b252e3d022629ee462
Harry Ma <hma41@jh.edu>
2026-01-25 15:24:54 -0600

    update init.py and port 8080 (was 8000)

--- module_1/backbone/__init__.py
+++ module_1/backbone/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask 
+from backbone.routes import bp 
+
 def create_app():
     app = Flask(__name__)
-    # .routes  = searching in folder vs global search 
-    from .routes import bp 
     app.register_blueprint(bp)
 
     return app
